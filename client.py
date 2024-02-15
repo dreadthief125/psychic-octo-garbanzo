1b4f97b9cfc4c3fe9ab8925b2f6f639bf8aa0eb2
dreadthief125 <88696805+dreadthief125@users.noreply.github.com>
2024-02-15 15:57:33 -0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+from threading import Thread
+IP_ADDRESS='127.0.0.1'
+PORT=8080
+SERVER=None
+clients={}
+
+def acceptConnections():
+    global SERVER
+    global clients
+    while True:
+        client,addr=SERVER.accept()
+        print(client,addr)
+def setup():
+    print('\n\t\t\t\t\t\tMUSIC SENDER\n')
+    global PORT
+    global IP_ADDRESS
+    global SERVER
+    SERVER=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS,PORT))
+    SERVER.listen(100)
+    print("\t\t\t\tSERVER IS WATING FOR INCOMMING CONNECTIONS...")
+    print("\n")
+    acceptConnections()
+setup_thread=Thread(target=setup)
+setup_thread.start()
